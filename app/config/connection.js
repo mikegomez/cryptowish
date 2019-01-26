2c9c6403986088a40b68e4970084e3091368376a
Mike Gomez <mikegomez21@hotmail.com>
2019-01-26 00:23:57 -0600

    update

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection ({
     host: 'localhost',
-    // port: '3306',
+     port: '3306',
     database: 'cTest',
     user: 'root',
     password: ""
